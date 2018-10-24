53295a437d1bf549979574615e11bd759e88121d
franknankivell <fn17aak@herts.ac.uk>
2018-10-24 18:49:10 +0100

    footer in place sort of

--- app_server/controllers/reporttool.js
+++ app_server/controllers/reporttool.js
@@ -3,3 +3,8 @@
 module.exports.home = function(req, res) {
   res.render('reporttool', {title: 'Report A Disaster Here'});
 };
+
+module.exports.info = function(req, res) {
+  res.render('reporttoolinfo', {title: 'Find Information about a disaster here'});
+
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -13,11 +13,15 @@ router.get('/learninghub/list',ctrllearninghub.list);
 router.get('/learninghub/comment',ctrllearninghub.comment);
 router.get('/learninghub/new',ctrllearninghub.new);
 
-// report roots
+// report routes
 router.get('/reporttool',ctrlreporttool.home);
+// reportinfo routes
+router.get('/reporttool/info', ctrlreporttool.info);
 // router etc
 
 // router for about
 router.get('/about',ctrlAbout.about);
+router.get('/privacy',ctrlAbout.privacy);
+router.get('/team',ctrlAbout.team);
 
 module.exports = router;

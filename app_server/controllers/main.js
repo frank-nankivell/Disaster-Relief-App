0df85e2b8ac839db34c41adc6a38087abb09f5a5
franknankivell <fn17aak@herts.ac.uk>
2019-01-06 21:26:34 +0000

    typo for url

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,7 +3,7 @@ var apiOptions = {
   server : "http://localhost:3000"
 };
 if (process.env.NODE_ENV === 'production') {
-  apiOptions.server = "https://www.rescueappinternational.com";
+  apiOptions.server = "https://secret-sands-46525.herokuapp.com/";
 }
 
 /* home page */

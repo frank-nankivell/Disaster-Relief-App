f17ef08d4925bcd181eac5a518c78213383783e8
franknankivell <fn17aak@herts.ac.uk>
2019-01-06 17:26:08 +0000

    updates again

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -1,7 +1,7 @@
 var passport = require('passport');
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
-var rT = mongoose.model('reportTool');
+var rT = mongoose.model('reporttool');
 
 var sendJSONresponse = function(res, status, content) {
     res.status(status);

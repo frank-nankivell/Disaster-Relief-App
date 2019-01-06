86c019432943aa7a0308a0e9fb4710584f3fff46
franknankivell <fn17aak@herts.ac.uk>
2019-01-06 20:35:45 +0000

    update

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -11,7 +11,6 @@ if (process.platform === "win32"){
         process.emit ("SIGINT");
     });
 }
-
 const dbURI = "mongodb://localhost:27017/DRAP";
 if (process.env.NODE_ENV === 'production') {
   dbURI = 'mongodb://admin:admin01@ds149894.mlab.com:49894/rescapp-01';
